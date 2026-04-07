2c2933fcbbe9ea28c966e4a34a3e1ea03d4b0167
AYUMI CHINTYA <ayumichintya1886@gmail.com>
2026-04-07 22:55:49 +0700

    praktikum 3

---
+++ resources/views/list_barang.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>List Barang</title>
+</head>
+<body>
+<h1>List Barang</h1>
+<p>ID Barang : {{ $id }}</p>
+<p>Nama Barang : {{ $nama }}</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,25 @@
 <?php
 use Illuminate\Support\Facades\Route; 
 use App\Http\Controllers\HomeController; 
- 
-//Route::get('/', function () { 
-  //  return view('welcome'); 
-//}); 
+use App\Http\Controllers\ListBarangController;
 
-Route::get('/', [HomeController::class, 'index']); 
-Route::get('/contact', [HomeController::class, 'contact']); 
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::get('/user/{id}', function ($id) {
+    return 'User ID : ' . $id;
+});
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return 'Admin Dashboard';
+    });
+    Route::get('/users', function () {
+        return 'Admin Users';
+    });
+});
+
+// Route::get('/listbarang/{id}/{nama}', function($id, $nama){
+//     return view('list_barang', compact('id', 'nama'));
+// });
+Route::get('/listbarang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
